0479a9a14ff054b8172394b85a1223e368e1b936
pimpbot9000 <t.valimaki@gmail.com>
2022-01-03 08:42:35 +0200

    Add github actions

--- create-gke-cluster.sh
+++ create-gke-cluster.sh
@@ -1,5 +1,5 @@
 gcloud container clusters create "sumdrill-cluster" \
-  --region "europe-north1" \
+  --zone "europe-north1-b" \
   --machine-type "e2-standard-2" --disk-type "pd-standard" --disk-size "100" \
   --num-nodes "1" --node-locations "europe-north1-a","europe-north1-b" \
   --release-channel=rapid
